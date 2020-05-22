55d45be5d8b3bf0e4e3cce0486e07056514faab8
Pila <pat@medicuja.com>
2020-05-22 10:36:26 +0200

    Set environment variable in webpack

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -11,7 +11,7 @@ const config = {
             sourceMap: true
         }),
         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production')
+            'process.env.NODE_ENV': JSON.stringify('prod')
         })
     ]
 };
