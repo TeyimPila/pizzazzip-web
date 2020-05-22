fdd3a670ca778d7d4a00f396023baf113fff079b
Pila <pat@medicuja.com>
2020-05-22 14:29:43 +0200

    debug 404 error on live

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,10 +1,18 @@
 const UglifyJsWebpackPlugin = require('uglifyjs-webpack-plugin');
 const webpack = require('webpack');
+const commonPaths = require('./common-paths');
 
 const config = {
     devtool: 'source-map',
     output: {
         publicPath: '/',
+    },
+    devServer: {
+        contentBase: commonPaths.outputPath,
+        compress: true,
+        historyApiFallback: true,
+        hot: false,
+        // port: 9000
     },
     plugins: [
         new UglifyJsWebpackPlugin({
