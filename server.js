c9b7c3c29282eb3fee4df4f92a6b2992fb92f55a
Pila <pat@medicuja.com>
2020-05-22 14:48:46 +0200

    debug 404 error on live

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static(__dirname));
 
 // send the user to index html page inspite of the url
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
