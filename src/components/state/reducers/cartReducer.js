291b90fdd5a8d99caca90c145f2e40dd20e3607e
Pila <pat@medicuja.com>
2020-05-23 20:10:07 +0200

    Implement logic for deleting items from cart in redux state (reducer)

--- src/components/state/reducers/cartReducer.js
+++ src/components/state/reducers/cartReducer.js
@@ -1,4 +1,5 @@
 import Types from '../actions/ActionTypes';
+import { remove } from 'lodash';
 
 export const initialState = {
     orderItems: [],
@@ -9,6 +10,15 @@ export default (state = initialState, action) => {
 
         case Types.ADD_TO_CART: {
             return { ...state, orderItems: [...state.orderItems, action.payload] };
+        }
+
+        case Types.EMPTY_CART: {
+            return { ...state, orderItems: [] };
+        }
+        case Types.DELETE_CART_ITEM: {
+            const orderItems = Object.assign([], state.orderItems);
+            remove(orderItems, { uuid: action.payload })
+            return { ...state, orderItems };
         }
         default:
             return state;
