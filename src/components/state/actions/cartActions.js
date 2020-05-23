6bb2389f84a5d32f8b0a08e6d76df8a53fb022b9
Pila <pat@medicuja.com>
2020-05-23 20:10:40 +0200

    Implement cart action creators

--- src/components/state/actions/cartActions.js
+++ src/components/state/actions/cartActions.js
@@ -5,3 +5,13 @@ export const addToCart = (orderItem) => ({
     type: Types.ADD_TO_CART,
     payload: orderItem
 });
+
+export const emptyCart = () => ({
+    type: Types.EMPTY_CART,
+    payload: {}
+});
+
+export const deleteCartItem = (uuid) => ({
+    type: Types.DELETE_CART_ITEM,
+    payload: uuid
+});
